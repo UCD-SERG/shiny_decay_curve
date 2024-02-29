b3b87869c966b2d31692ab052fb894b820aad0aa
Chris Orwa <chrisorwa@gmail.com>
2024-02-29 22:32:00 +0300

    add new files

---
+++ server.R
@@ -0,0 +1,40 @@
+
+# load script
+source('graph.decay.curves.R')
+
+# Define server logic for slider examples ----
+server <- function(input, output,session) {
+
+  # Reactive expression to create data frame of all input values ----
+  sliderValues <- reactive({
+
+    data.frame(
+
+      matrix(data = c(input$y0,
+                      input$y1,
+                      input$t1,
+                      input$alpha,
+                      input$r),nrow = 1,ncol = 5) %>%
+        set_colnames(c("y0",
+                       "y1",
+                       "t1",
+                       "alpha",
+                       "r"))
+    )
+  })
+
+  # Show the values in an HTML table ----
+  output$values <- renderTable({
+    sliderValues()
+  })
+
+  observeEvent(input$y1, {
+    updateSliderInput(inputId = "y1", max = input$y0)
+  })
+
+  output$plot <- renderPlot({
+    # plot curve
+    p = plot_curve_params_one_ab(object = sliderValues())
+    print(p)
+  }, res = 96)
+}
