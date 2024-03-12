321d5ee7a575687f274c376e9b0e0af9cbffcab9
Chris Orwa <chrisorwa@gmail.com>
2024-03-12 22:56:55 +0300

    change title

--- ui.R
+++ ui.R
@@ -8,7 +8,7 @@ library(magrittr)
 ui <- fluidPage(
 
   # App title ----
-  titlePanel("Parameters"),
+  titlePanel("Serocalculator: Interactive Antibody Kinetic Model"),
 
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -19,27 +19,27 @@ ui <- fluidPage(
       # Input: Simple integer interval ----
       sliderInput("y0", "Baseline antibody concentration (y0):",
                   min = 0.0, max = 1000,
-                  value = 2.7,step = 10),
+                  value = 2.7,step = 0.01),
 
       # Input: Decimal interval with step value ----
       sliderInput("y1", "Peak antibody concentration (y1):",
                   min = 0.5, max = 1200,
-                  value = 63.48, step = 100),
+                  value = 63.48, step = 0.01),
 
       # Input: Specification of range within an interval ----
       sliderInput("t1", "Time to peak antibody concentration (t1):",
                   min = 0.0, max = 100,
-                  value = 9.51, step = 0.1),
+                  value = 9.51, step = 0.0001),
 
       # Input: Custom currency format for with basic animation ----
       sliderInput("alpha", "Antibody decay rate in days (alpha):",
-                  min = 5.494682e-08, max = 0.35,
-                  value =0.1, step = 0.1),
+                  min = 5.494682e-08, max = 0.35, # 0.0001
+                  value =0.1, step = 0.0001),
 
       # Input: Animation
       sliderInput("r", "Antibody decay shape (r):",
                   min = 1.0, max = 19,
-                  value = 1.7, step = 1)
+                  value = 1.7, step = 0.01)
 
     ),
 
