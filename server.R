872ea403d329993ebba9137fbf23bdf3536f12b2
Douglas Ezra Morrison <demorrison@ucdavis.edu>
2024-03-26 18:11:24 -0400

    finished

--- server.R
+++ server.R
@@ -11,30 +11,43 @@ server <- function(input, output,session) {
   # Reactive expression to create data frame of all input values ----
   sliderValues <-
     data.frame(
-      y0 = input$y0,
+      y0 = 10^input$y0,
       y1 = 10^input$y1,
-      t1 = input$t1,
+      t1 = 10^input$t1,
       alpha = 10^input$alpha,
-      r = input$r) |>
+      r = 10^input$r) |>
     reactive()
 
   # Show the values in an HTML table ----
   output$values <- sliderValues() |> renderTable()
 
+  # see https://plannapus.github.io/blog/2021-05-27.html:
+  session$onFlushed(
+    function() shinyjs::runjs("logifySlider('y0', sci = true);")
+    ,
+    once=FALSE)
   # Limit y1 value to maximum of y0
-  updateSliderInput(
-    session,
-    inputId = "y0",
-    max = input$y1) |>
-  observeEvent(eventExpr = input$y0)
+  {
 
-output$plot <-
-  serocalculator:::plot_curve_params_one_ab(
-    object = sliderValues(),
-    xlim = c(1/24, max(100, input$t1*2)),
-    n_points = 10^input$n_pts,
-    log_x = input$log_x,
-    log_y = input$log_y) |>
-  renderPlot(res = 96)
+    updateSliderInput(
+      session,
+      inputId = "y0",
+      max = input$y1)
+  }|>
+    observeEvent(eventExpr = input$y1)
+
+  output$plot <-
+    {
+      serocalculator:::plot_curve_params_one_ab(
+        object = sliderValues(),
+        n_points = 10^input$n_pts,
+        log_x = FALSE,
+        log_y = FALSE) +
+        scale_y_continuous(limits = c(NA, input$y_max),
+                           trans = ifelse(input$log_y, "log10", "identity")) +
+        scale_x_continuous(limits = c(1/24, input$x_max),
+                           trans = ifelse(input$log_x, "log10", "identity"))
+    }|>
+    renderPlot(res = 96)
 
 }
--- ui.R
+++ ui.R
@@ -28,24 +28,20 @@ JS.onload <-
 $(document).ready(function() {
   // wait a few ms to allow other scripts to execute
   setTimeout(function() {
-    // include call for each slider
-logifySlider('y0', sci = true)
-logifySlider('y1', sci = true)
-logifySlider('alpha', sci = false)
-logifySlider('r', sci = false)
-logifySlider('n_pts', sci = false, sigfig = 0)
-  }, 5);
-$('#y1').onFinish(function() {
-setTimeout(function() {
-// include call for each slider
-logifySlider('y0', sci = true)
-}, 100);
-})
+
+  // include call for each slider
+  logifySlider('y0', sci = true)
+  logifySlider('y1', sci = true)
+  logifySlider('t1', sci = false, sigfig = 0)
+  logifySlider('alpha', sci = false)
+  logifySlider('r', sci = false, sigfig = 2)
+  logifySlider('n_pts', sci = false, sigfig = 0)
+    }, 5);
 })
 "
 # Define UI for app --
 ui <- fluidPage(
-
+  shinyjs::useShinyjs(),
   # App title ----
   titlePanel("Serocalculator: Interactive Antibody Kinetic Model"),
 
@@ -57,38 +53,43 @@ ui <- fluidPage(
 
       tags$head(tags$script(HTML(JS.logify))),
       tags$head(tags$script(HTML(JS.onload))),
-
-      actionButton("C0_widget_setval", "Set this value"),
-      sliderInput("y0", "Baseline antibody concentration (y0):",
+      shiny::withMathJax(),
+      sliderInput("y0", "Baseline antibody concentration \\((y_{0})\\):",
                   min = .001 |> log10(),
                   max = 1000 |> log10(),
                   value = 10 |> log10(),
-                  step = 0.01),
+                  step = 0.1),
 
       # Input: Decimal interval with step value ----
-      sliderInput("y1", "Peak antibody concentration (y1):",
+      sliderInput("y1", "Peak antibody concentration \\((y_{1})\\):",
                   min = 100 |> log10(),
                   max = (10^6) |> log10(),
                   value = 10000 |> log10(),
-                  step = .01),
+                  step = .1),
 
       # Input: Specification of range within an interval ----
-      sliderInput("t1", "Time to peak antibody concentration (t1):",
-                  min = 0.0, max = 200,
-                  value = 9.51, step = 0.125),
+      sliderInput("t1", "Time to peak antibody concentration \\((t_1)\\):",
+                  min = (1) |> log10(),
+                  max = 200 |> log10(),
+                  value = 9.51 |> log10(),
+                  step = 0.01,
+                  post = " days"),
 
       # Input: Custom currency format for with basic animation ----
       sliderInput("alpha",
-                  "Antibody decay rate in days (alpha):",
+                  "Antibody decay rate in days \\((\\alpha)\\):",
                   min = 0.0001 |> log10(),
                   max = 1 |> log10(),
-                  value = 0.1 |> log10(),
+                  value = 0.01 |> log10(),
                   step = 0.01),
 
       # Input: Animation
-      sliderInput("r", "Antibody decay shape (r):",
-                  min = 1.0, max = 3,
-                  value = 1.7, step = 0.01),
+      sliderInput("r",
+                  "Antibody decay shape \\((r)\\):",
+                  min = 1.0 |> log10(),
+                  max = 3 |> log10(),
+                  value = 1.7 |> log10(),
+                  step = 0.01),
 
       sliderInput("n_pts", "# curve graphing points:",
                   min = 1,
@@ -104,17 +105,34 @@ ui <- fluidPage(
     mainPanel(
 
       # Output: Table summarizing the values entered ----
-      shiny::checkboxInput(
-        inputId = "log_y",
-        label = "logarithmic y-axis",
-        value = FALSE),
+      fluidRow(
+        column(
+          width = 4,
+          shiny::numericInput(
+            "y_max",
+            label = "y-axis maximum",
+            value = 10000),
+          shiny::checkboxInput(
+            inputId = "log_y",
+            label = "logarithmic y-axis",
+            value = FALSE)
+        )),
       plotOutput("plot", click = NULL),
-      shiny::checkboxInput(
-        inputId = "log_x",
-        label = "logarithmic x-axis",
-        value = FALSE) |>
-        shiny::column(offset = 8, width = 4) |>
-        shiny::fluidRow(),
+      shiny::fluidRow(
+
+        shiny::checkboxInput(
+          inputId = "log_x",
+          label = "logarithmic x-axis",
+          value = FALSE) |>
+          shiny::column(offset = 5, width = 3),
+
+        shiny::numericInput(
+          "x_max",
+          label = "x-axis maximum",
+          value = 100) |>
+          column(width = 4)
+      ),
+
       tableOutput("values")
 
     )
